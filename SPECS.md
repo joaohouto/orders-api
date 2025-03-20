b552fbdb51ef51ce09887933b7c123e34ef199a1
João Couto <joaocouto.jar@gmail.com>
2025-03-20 15:09:39 -0400

    idk

---
+++ SPECS.md
@@ -0,0 +1,25 @@
+# Application Specifictions
+
+- Users must be able to sign up/in;
+- Users must be able to mange their profiles (name, photo)
+
+- Users must be able to create their own Pages;
+- User that created a page is its owner;
+- Owner can add collaborators (users) to his page;
+
+- Pages have name, a icon url and banner url;
+- Pages have products;
+
+- Products have name, images, price;
+- Products may have variations;
+- Variations have name and price;
+
+- Orders have a user, a page relation, the product ordered and its variation ordered;
+
+## Entities
+
+- User
+- Page
+  - Files
+  - Products
+  - Orders
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 import express, { Request, Response } from "express";
 
+import { ExpressAuth } from "@auth/express"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "@/database/prisma"
+import Google from "@auth/express/providers/google"
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.set("trust proxy", true)
+app.use(
+  "/auth/*",
+  ExpressAuth({
+    providers: [Google],
+    adapter: PrismaAdapter(prisma),
+  })
+)
+
+// callback url [origin]/auth/callback/google
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to the Node.js + TypeScript API!");
